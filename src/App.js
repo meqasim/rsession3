8095a8625622b16b92c6fcc673347ce55fbe3a55
HaFiZ CH QaSiM <63259297+meqasim@users.noreply.github.com>
2021-10-05 16:58:46 +0500

    Updated app.js file

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         React App Session 3
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.blockchain313.com"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          BlockChain Technology
         </a>
       </header>
     </div>
