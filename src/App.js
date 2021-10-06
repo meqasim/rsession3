d4b1eb03b013a3ce8abe1bf99d6f8a2e102ea7e5
HaFiZ CH QaSiM <63259297+meqasim@users.noreply.github.com>
2021-10-06 16:02:30 +0500

    testing 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         React App Session 3  3216549871 changes
+         React App Session 3  3216549871
+        </p>
+        <p>
+         testing 2
         </p>
         <a
           className="App-link"
